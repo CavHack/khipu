4d35d0cd0ba3cc91d9cde6701bd38a465a5142f3
Karl Whitford <karl@blackesquirel.com>
2019-10-15 23:58:33 -0400

    Filtering params update
    Represents the amount of filtering that can be done to streams on Twitter's side. According to Twitter's documentation, "When displaying a stream of Tweets to end users (dashboards or live feeds at a presentation or conference, for example) it is suggested that you set this value to medium. Custom is convenient to further enhance filtering abilities and adapt NLU properties here in the future

--- src/filter.rs
+++ src/filter.rs
@@ -1,23 +1,89 @@
 //! Common filters used across the crate.
 
+/! Common filters used across the crate.
+
+//! Access to the Streaming API.
+//!
+//! The Streaming API gives real-time access to tweets, narrowed by
+//! search phrases, user id or location. A standard user is able to filter by up to
+//! 400 keywords, 5,000 user ids and 25 locations.
+//! See the [official documentation](https://developer.twitter.com/en/docs/tweets/filter-realtime/overview) for more details.
+//!
+//! ### Example
+//! ```rust,no_run
+//! # fn main() {
+//! # let token: egg_mode::Token = unimplemented!();
+//! use egg_mode::stream::{filter, StreamMessage};
+//! use tokio::runtime::current_thread::block_on_all;
+//! use futures::Stream;
+//!
+//! let stream = filter()
+//!     // find tweets mentioning any of the following:
+//!     .track(&["rustlang", "python", "java", "javascript"])
+//!     .start(&token);
+//!
+//! block_on_all(stream.for_each(|m| {
+//!     // Check the message type and print tweet to console
+//!     if let StreamMessage::Tweet(tweet) = m {
+//!         println!("Received tweet from {}:\n{}\n", tweet.user.unwrap().name, tweet.text);
+//!     }
+//!     futures::future::ok(())
+//! })).expect("Stream error");
+//! # }
+//! ```
+//! ### Connection notes
+//! To maintain a stable streaming connection requires a certain amount of effort to take
+//! account of random disconnects, networks resets and stalls. The key points are:
+//!
+//! * The Twitter API sends a Ping message every 30 seconds of message inactivity. So set a timeout
+//! such that after (say) 1 minute of inactivity, the client bounces the connection. This will protect
+//! against network stalls
+//! * Twitter will rate-limit reconnect attempts. So attempt conenctions with a linear or exponential
+//! backoff strategy
+//! * In the case of an unreliable connection (e.g. mobile network), fall back to the polling API
+//!
+//! The [official guide](https://developer.twitter.com/en/docs/tweets/filter-realtime/guides/connecting) has more information.
+use std::collections::HashMap;
+use std::str::FromStr;
+use std::{self, io};
+use khipu::{rt, Credentials, Glyph};
+
+
 pub use hyper::Method as RequestMethod;
 pub use hyper::StatusCode;
 pub use hyper::Uri;
 
 string_enums! {
+/// Represents the amount of filtering that can be done to streams on Twitter's side.
+///
+/// According to Twitter's documentation, "When displaying a stream of Tweets to end users
+/// (dashboards or live feeds at a presentation or conference, for example) it is suggested that
+/// you set this value to medium."
     /// Represents the `filter_level` parameter in API requests.
     #[derive(Clone, Debug)]
     pub enum FilterLevel {
-        None("none"),
+ /// No filtering.
+    #[serde(rename = "none")]
+	None("none"),
+/// A light amount of filtering.
+    #[serde(rename = "low")]
         Low("low"),
+   /// A medium amount of filtering.
+    #[serde(rename = "medium")]
         Medium("medium");
         Custom(_),
     }
 }
 
-impl std::default::Default for FilterLevel {
-    fn default() -> Self {
-        FilterLevel::None
+/// `Display` impl to turn `FilterLevel` variants into the form needed for stream parameters. This
+/// is basically "the variant name, in lowercase".
+// TODO Probably can remove this somehow
+impl ::std::fmt::Display for FilterLevel {
+    fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+        match *self {
+            FilterLevel::None => write!(f, "none"),
+            FilterLevel::Low => write!(f, "low"),
+            FilterLevel::Medium => write!(f, "medium"),
+        }
     }
-}
 
